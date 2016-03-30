3547866803631dcf36653f2fed5528458742b605
Adrián Víctor Poplavsky <adrianpoplavsky@hotmail.com>
2016-03-29 23:53:14 -0300

    Initial commit.

---
+++ RedisWrapper/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace RedisWrapper.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ RedisWrapper/Controllers/EntitiesController.cs
@@ -0,0 +1,43 @@
+﻿using DataCacheService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RedisWrapper.Controllers
+{
+    public class EntitiesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+
+           return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public Employee Get(string setName, int id)
+        {
+            return Program.Get(setName, id);
+        }
+
+        // POST api/values
+        public void Post(string setName, [FromBody]Employee employee)
+        {
+            Program.Set(setName, employee);
+        }
+
+        //// PUT api/values/5
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+            Program.Remove(id);
+        }
+    }
+}
